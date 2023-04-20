c40988e7babc3974825d526dbc601a72ed927683
4Gettt25 <felix.guenther24@gmail.com>
2023-04-20 14:26:42 +0200

    small changes

--- Programmieren/java/Java-Basics/local-time.java
+++ Programmieren/java/Java-Basics/local-time.java
@@ -2,11 +2,10 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 class Programm2 {
-  public static void main(String[] args) {
-   LocalDate LocalDatejetzt = LocalDate.now();
-    DateTimeFormatter meinFormat = DateTimeFormatter.ofPattern(
-        "EEEE, d. MMMM yyyy");
-    System.out.println("Heute ist " + meinFormat.format(LocalDatejetzt) + "!");
-    System.out.println("Willkommen zu OOP" + "!");
-  }
+ public static void main(String[] args) {
+  LocalDate jetzt = LocalDate.now();
+  DateTimeFormatter meinFormat = DateTimeFormatter.ofPattern("EEEE, d. MMMM yyyy");
+  System.out.println("Heute ist " + meinFormat.format(jetzt) + "!");
+  System.out.println("Willkommen zu OOP" + "!");
+ }
 }
